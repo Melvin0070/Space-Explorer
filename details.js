1789d87dd56991bdf4cdaa4fd31cf86e8fbcd2f7
Melvin0070 <melvin.aspirant@gmail.com>
2023-09-09 00:43:36 +0530

    M4 submit

--- details.js
+++ details.js
@@ -3,6 +3,11 @@ const usernameInput = document.getElementById('usernameInput');
 const nicknameInput = document.getElementById('nicknameInput');
 const form = document.getElementById('form'); //Bug fix by GPT, (button -> form)
 
+// Javascript Object to store name abd nick name
+let player = {
+
+}
+
 form.addEventListener('submit', function() {
 
     const username = usernameInput.value;
@@ -10,7 +15,15 @@ form.addEventListener('submit', function() {
 
     localStorage.setItem('username', username);
     localStorage.setItem('nickname', nickname);
+    localStorage.setItem('playerInfo', player)
+
+    player = {
+        username: username,
+        nickname: nickname
+    }
+    
 });
+
 const bgMuisc = new Audio('./assets/cosmos-space-game-action-shooter-astronauts-scifi-aliens-142978.mp3')
 bgMuisc.play()
 bgMuisc.loop = true
